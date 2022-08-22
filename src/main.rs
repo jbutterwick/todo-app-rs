cb9a937423dae4d810a77a57e4ba2d43dab619b7
jordanbutterwick <jordan.butterwick@statflo.com>
2022-08-22 15:30:08 -0600

    refactor main dispatcher for clarity and conciseness

--- src/main.rs
+++ src/main.rs
@@ -62,11 +62,10 @@ fn dispatch(todo: &mut Todo, input: String) -> Output<String> {
 	let trimmed = input.trim();
 	let lowercase = trimmed.to_lowercase();
 	let commands = lowercase.split_whitespace().collect::<Vec<&str>>();
-
-	match &commands[..] {
-		&[first] => match first {
-			"help" => HelpResponse {
-				help_msg: "
+	let (first, tail) = commands.split_first().unwrap();
+	match **&first {
+		"help" => HelpResponse {
+			help_msg: "
                       Available commands:
                       help                              Displays this help
                       list                              Display the todo list
@@ -74,73 +73,60 @@ fn dispatch(todo: &mut Todo, input: String) -> Output<String> {
                       done <todo item number>           Marks the item as done
                       quit                              Exit the program
 					",
-			}
-			.to_output(),
-			"list" => ListResponse {
+		}
+		.to_output(),
+
+		"list" => ListResponse {
+			list: &todo.item_list,
+		}
+		.to_output(),
+
+		"quit" | "exit" | "q" | "e" => ExitResponse {
+			exit_msg: "buh-bye!",
+		}
+		.to_output(),
+
+		"add" => {
+			let string_tail = tail.join(" ");
+			todo.item_list.items.push(Item::from(&*string_tail));
+			todo.item_list.items.sort();
+			ListResponse {
 				list: &todo.item_list,
 			}
-			.to_output(),
-			"quit" | "exit" => ExitResponse {
-				exit_msg: "buh-bye!",
-			}
-			.to_output(),
-			"add" | "done" => ErrorResponse {
-				error_msg: "not enough arguments provided for this command",
-			}
-			.to_output(),
-			_ => ErrorResponse {
-				error_msg: "unknown argument",
-			}
-			.to_output(),
-		},
-		[first, tail @ ..] => match first {
-			&"add" => {
-				let string_tail = tail.join(" ");
-				todo.item_list.items.push(Item::from(&*string_tail));
-				todo.item_list.items.sort();
-				ListResponse {
-					list: &todo.item_list,
+		}
+		.to_output(),
+
+		"done" => {
+			let string_tail = tail.join(" ");
+			let index = match todo
+				.item_list
+				.items
+				.binary_search(&Item::from(&*string_tail))
+			{
+				Ok(index) => index,
+				_ => {
+					string_tail
+						.parse::<usize>()
+						.expect("unable to find index by parse or search")
+						- 1
 				}
-			}
-			.to_output(),
-			&"done" => {
-				let string_tail = tail.join(" ");
+			};
 
-				let index = todo
-					.item_list
-					.items
-					.binary_search(&Item::from(&*string_tail))
-					.unwrap_or(
-						string_tail
-							.parse::<usize>()
-							.expect("unable to find index by parse or search")
-							- 1,
-					);
+			let mut item = todo
+				.item_list
+				.items
+				.get_mut(index)
+				.expect("index out of bounds");
 
-				let mut item = todo
-					.item_list
-					.items
-					.get_mut(index)
-					.expect("index out of bounds");
+			item.state = State::Done;
+			ListResponse {
+				list: &todo.item_list,
+			}
+		}
+		.to_output(),
 
-				item.state = State::Done;
-				ListResponse {
-					list: &todo.item_list,
-				}
-			}
-			.to_output(),
-			&"help" | &"list" | &"quit" => ErrorResponse {
-				error_msg: "unexpected argument",
-			}
-			.to_output(),
-
-			_ => ErrorResponse {
-				error_msg: "unknown argument",
-			}
-			.to_output(),
-		},
-		[] => ErrorResponse {
-			error_msg: "no argument made. type `help`",
+		_ => ErrorResponse {
+			error_msg: "unknown argument",
 		}
 		.to_output(),
 	}
